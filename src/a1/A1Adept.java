be5f1c3989720ab97ab1142b0dbf689f85dcc388
kodyrobinson <kodyrobinson@100.64.10.245>
2019-08-26 18:36:02 -0400

    Deleted the hashmap, it was just not a good idea.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -2,9 +2,6 @@ package a1;
 
 import java.util.Scanner;
 
-// the hashmap utility imported below is for experimental purposes only
-import java.util.HashMap;
-
 public class A1Adept {
 
 	public static void main(String[] args) {
@@ -14,17 +11,13 @@ public class A1Adept {
 		// Your code follows here.
 		int num_of_store_items = scan.nextInt();
 		
-		// Create a hashmap (for temporary experimental purposes)
-		
-		HashMap<Integer, String> item_prices = new HashMap<Integer, String>();
 		
 		
 		// Insert a for loop to iterate through all the items, storing the names as strings and their prices
 		for (int i = 0; i < num_of_store_items; i++) {
 			String item_name = scan.next();
 			double item_price = scan.nextDouble();
-			item_prices.put(Integer.valueOf(item_name), String.format("%.2f", item_price));
-			System.out.println(item_prices.get(Integer.valueOf(item_name)));
+			
 			
 		}
 	}
