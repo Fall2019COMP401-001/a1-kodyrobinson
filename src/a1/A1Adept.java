b79327e6e7ca0b0d785f5a110966e5ab533cc51c
kodyrobinson <kodyrobinson@100.64.10.245>
2019-08-28 11:20:33 -0400

    Working on converting strings to string arrays

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Adept {
@@ -11,44 +12,58 @@ public class A1Adept {
 		// Your code follows here.
 		int num_of_store_items = scan.nextInt();
 		
-		// This is an experimental scanner used to scan a string concatenated to store all the items and their prices for future reference
-		// Scanner scan2 = new Scanner()
-		String items = "";
+		// Create an array of strings to store all info that must be referenced later
+		String[] items;
 		// Insert a for loop to iterate through all the items, storing the names as strings and their prices
 		for (int i = 0; i < num_of_store_items; i++) {
 			String item_name = scan.next();
 			double item_price = scan.nextDouble();
-			items = items + item_name + String.format("%.3f", item_price);
+			items = append(items, item_name);
+			items = append(items, String.format("%.2f", item_price));
 		}
 		int num_of_customers = scan.nextInt();
+				
 		
-		String cust_names_and_totals = "";
+		// these strings will need to become arrays
+		String[] cust_names_and_totals;
 		
-		String list_of_total_prices = "";
+		String[] list_of_total_prices;
 		
 		double avg = 0.0;
+		
+		// iteration through the input. leave this but change it up to work with arrays instead of strings
 		
 		for (int i = 0; i < num_of_customers; i++) {
 			String specific_cust_first = scan.next();
 			String specific_cust_last = scan.next();
-			cust_names_and_totals = cust_names_and_totals + specific_cust_first + " " + specific_cust_last + " ";
+			cust_names_and_totals = append(cust_names_and_totals, specific_cust_first);
+			cust_names_and_totals = append(cust_names_and_totals, specific_cust_last);
 			int num_of_items = scan.nextInt();
 			double cust_total_price = 0.0;
 			
 			for (int j = 0; j < num_of_items; j++) {
 				int num_of_specific_item = scan.nextInt();
 				String specific_item = scan.next();
-				int price_index = items.indexOf(specific_item) + specific_item.length();
-				String price_of_specific_item = items.substring(price_index, price_index + 5);
+				int k = 0;
+				while (!(items[k].contentEquals(specific_item))) {
+					k += 1;
+				}
+				int price_index = k + 1;
+				String price_of_specific_item = items[price_index];
 				Scanner scan_price = new Scanner(price_of_specific_item);
 				double price = scan_price.nextDouble();
 				double item_tot_price = num_of_specific_item * price;
 				cust_total_price += item_tot_price;
 				
 			}
-			list_of_total_prices = list_of_total_prices + String.format("%.2f", cust_total_price) + " ";
+			cust_names_and_totals = append(cust_names_and_totals, String.format("%.2f", cust_total_price));
+			list_of_total_prices = append(list_of_total_prices, String.format("%.2f", cust_total_price));
 			
 		}
+		System.out.println(items);
+		System.out.println(cust_names_and_totals);
+		System.out.println(list_of_total_prices);
+		// delete these scanners and heavily review the following code
 		
 		Scanner scan_list = new Scanner(list_of_total_prices);
 		
@@ -89,12 +104,17 @@ public class A1Adept {
 			max_price = scan_price_list.nextDouble();
 		}
 		
-		System.out.println(max_price);
+		
 		
 		
 		
 		avg /= num_of_customers;
 		
+	}
+	static String[] append(String[] a, String b) {
+		String[] temp = Arrays.copyOf(a, a.length+1);
+		temp[temp.length-1] = b;
+		return temp;
 	}
 
 	// 2 potential ideas: either create a string[] with all the pricing data or create one concatenated string containing
